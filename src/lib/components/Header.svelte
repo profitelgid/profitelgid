975ceebab07c40b6dddbbcfbed060140c2ba93ba
@kaspar <team@crewnew.com>
2023-01-19 19:21:47 +0000

    fix: top menu

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,213 +1,201 @@
 <div class="header">
-	<h1 class="logo">
-		<strong>Telkide, peoinventaari ja lavade müük ja rent.</strong>
-		<a
-			href="http://profitelgid.ee/est"
-			title="Telkide, peoinventaari ja lavade müük ja rent."
-			class="logo"
-		>
-			<img
-				src="http://profitelgid.ee/skin/frontend/default/hellowired/images/logo.gif"
-				alt="Telkide, peoinventaari ja lavade müük ja rent."
-			/></a
-		>
-	</h1>
-	<div class="cms-links">
-		<ul>
-			<li class="welcome">hulgi@profitelgid.ee</li>
-			<a
-				href="http://profitelgid.ee/est/abs-invest/sisene.html"
-				title="Püsiklinedi kontohaldus ja tellimine">Sisene>></a
-			>
-		</ul>
-	</div>
+  <h1 class="logo">
+    <strong>Telkide, peoinventaari ja lavade müük ja rent.</strong>
+    <a
+      href="http://profitelgid.ee/est"
+      title="Telkide, peoinventaari ja lavade müük ja rent."
+      class="logo"
+    >
+      <img
+        src="http://profitelgid.ee/skin/frontend/default/hellowired/images/logo.gif"
+        alt="Telkide, peoinventaari ja lavade müük ja rent."
+      /></a
+    >
+  </h1>
+  <div class="cms-links">
+    <ul>
+      <li class="welcome">hulgi@profitelgid.ee</li>
+      <a
+        href="http://profitelgid.ee/est/abs-invest/sisene.html"
+        title="Püsiklinedi kontohaldus ja tellimine">Sisene>></a
+      >
+    </ul>
+  </div>
 </div>
 <div id="navigation">
-	<div class="nav-container">
-		<ul id="navbar">
-			<li>
-				<a href="http://profitelgid.ee/est/">
-					<span>Avaleht</span>
-				</a>
-			</li>
-			<li class="dropdown">
-				<a href="/">
-					<span>Telkide &amp; peoinventari rent</span>
-					<img alt="clickable" src="/src/assets/images/menu_bullet.png" style="margin-left: 5px;" />
-					<div class="arrow" />
-				</a>
-				<ul>
-					<div class="dropdown-content">
-						<li>
-							<a href="http://profitelgid.ee/est/telkide-laenutus/odav-telgi-rent.html">
-								<span>Telkide laenutus</span>
-							</a>
-						</li>
-						<li>
-							<a href="http://profitelgid.ee/est/telkide-laenutus/peoinventar-56.html">
-								<span>Peoinventari rent</span>
-							</a>
-						</li>
-						<li>
-							<a href="http://profitelgid.ee/est/telkide-laenutus/heli-foto-video-teenused.html">
-								<span>Heli, foto, video</span>
-							</a>
-						</li>
-					</div>
-				</ul>
-			</li>
-			<li class="dropdown">
-				<a href="/">
-					<span>Telkide &amp; peoinventari müük</span>
-					<img alt="clickable" src="/src/assets/images/menu_bullet.png" style="margin-left: 5px;" />
-				</a>
-				<ul>
-					<div class="dropdown-content">
-						<!--
+  <div class="nav-container">
+    <ul id="navbar">
+      <li>
+        <a href="http://profitelgid.ee/est/">
+          <span>Avaleht</span>
+        </a>
+      </li>	
+      <li class="dropdown">
+        <a href="/">
+          <span>Telkide &amp; peoinventari rent</span>
+          <img alt="clickable" src="/src/assets/images/menu_bullet.png" style="margin-left: 5px;">
+          <div class="arrow"></div>
+        </a>
+        <ul>
+          <div class="dropdown-content">
+            <li>
+              <a href="http://profitelgid.ee/est/telkide-laenutus/odav-telgi-rent.html">
+                <span>Telkide laenutus</span>
+              </a>
+            </li>
+            <li>
+              <a href="http://profitelgid.ee/est/telkide-laenutus/peoinventar-56.html">
+                <span>Peoinventari rent</span>
+              </a>
+            </li>
+            <li>
+              <a href="http://profitelgid.ee/est/telkide-laenutus/heli-foto-video-teenused.html">
+                <span>Heli, foto, video</span>
+              </a>
+            </li>
+          </div>
+        </ul>
+      </li>
+      <li class="dropdown">
+        <a href="/">
+          <span>Telkide &amp; peoinventari müük</span>
+          <img alt="clickable" src="/src/assets/images/menu_bullet.png" style="margin-left: 5px;">
+        </a>
+        <ul>
+          <div class="dropdown-content">
+            <!--
               <li>
                 <a href="http://profitelgid.ee/est/telkide-myyk/soodus.html">
                   <span>PAKKUMISED</span>
                 </a>
               </li>
             -->
-						<li>
-							<a href="http://profitelgid.ee/est/telkide-myyk/kasutatud-kuulutused.html">
-								<span>Leiunurk, kasutatud </span>
-							</a>
-						</li>
-						<li>
-							<a href="http://profitelgid.ee/est/telkide-myyk/moodul-pro.html">
-								<span>EasyFlex PRO moodultelgid</span>
-							</a>
-						</li>
-						<li>
-							<a href="http://profitelgid.ee/est/telkide-myyk/profitelk-muuk.html">
-								<span>Tugevad profitelgid</span>
-							</a>
-						</li>
-						<li class="dropdown">
-							<a href="http://profitelgid.ee/est/telkide-myyk/pop-up.html">
-								<span>Pop-up kergtelgid</span>
-							</a>
-							<span class="arrow" lang="6"
-								><img
-									alt="clickable"
-									src="/src/assets/images/menu_bullet.png"
-									style="float:right; transform: rotate(270deg);"
-								/></span
-							>
-							<ul>
-								<div class="dropdown-content dropdown-content-2">
-									<li class="">
-										<a href="http://profitelgid.ee/est/telkide-myyk/pop-up/popup-koduaeda.html">
-											<span>Home soodne pop-up</span>
-										</a>
-									</li>
-									<li class="">
-										<a href="http://profitelgid.ee/est/telkide-myyk/pop-up/soodsad-easyup.html">
-											<span>Classic Alu kergtelgid</span>
-										</a>
-									</li>
-									<li class="">
-										<a href="http://profitelgid.ee/est/telkide-myyk/pop-up/premium-popup.html">
-											<span>Premium pop-up telgid</span>
-										</a>
-									</li>
-									<li class="">
-										<a
-											href="http://profitelgid.ee/est/telkide-myyk/pop-up/kuuskant-hex-profi-popup.html"
-										>
-											<span>PRO seeria popup telgid</span>
-										</a>
-									</li>
-								</div>
-							</ul>
-						</li>
-						<li>
-							<a href="http://profitelgid.ee/est/telkide-myyk/umartelgid.html">
-								<span>Ümartelgid &amp; star-telgid</span>
-							</a>
-						</li>
-						<li>
-							<a href="http://profitelgid.ee/est/telkide-myyk/peoinventar.html">
-								<span>Peoinventar</span>
-							</a>
-						</li>
-						<li>
-							<a href="http://profitelgid.ee/est/telkide-myyk/telkide-lisavarustus.html">
-								<span>Telkide lisavarustus</span>
-							</a>
-						</li>
-						<li>
-							<a href="http://profitelgid.ee/est/telkide-myyk/teisaldatav-garaaz-telk.html">
-								<span>Teisaldatavad garaaž-telgid</span>
-							</a>
-						</li>
-						<li class="dropdown">
-							<a href="http://profitelgid.ee/est/telkide-myyk/laotelgid.html">
-								<span>Laotelgid &amp; tulekindlad telgid</span>
-							</a>
-							<span class="arrow" lang="6"
-								><img
-									alt="clickable"
-									src="/src/assets/images/menu_bullet.png"
-									style="margin-left: 5px; float:right; transform: rotate(270deg);"
-								/></span
-							>
-							<ul>
-								<div class="dropdown-content dropdown-content-2">
-									<li>
-										<a href="https://profitelgid.ee/est/telkide-myyk/profitelk-muuk.html">
-											<span>A-tüüpi laotelgid</span>
-										</a>
-									</li>
-									<li>
-										<a
-											href="http://profitelgid.ee/est/telkide-myyk/laotelgid/vastupidav-laotelk.html"
-										>
-											<span>B-tüüpi laotelgid</span>
-										</a>
-									</li>
-								</div>
-							</ul>
-						</li>
-						<li>
-							<a href="http://profitelgid.ee/est/telkide-myyk/pvc-hallid-kaarhallid.html">
-								<span>PVC hallid, kaarhallid</span>
-							</a>
-						</li>
-					</div>
-				</ul>
-			</li>
-			<li>
-				<a href="http://profitelgid.ee/est/vaba-aeg/hobispordi-kaubad.html"> Hobispordi kaubad </a>
-			</li>
-			<li>
-				<a href="http://www.profitelgid.ee/est/abs-invest">Kontakt</a>
-			</li>
-			<li>
-				<a href="/">
-					<span>Kodu &amp; aed</span>
-				</a>
-				<ul>
-					<li>
-						<a href="http://profitelgid.ee/est/vaba-aeg/kasvuhooned.html">
-							<span>Kasvuhooned</span>
-						</a>
-					</li>
-					<li>
-						<a href="http://profitelgid.ee/est/vaba-aeg/metall.html">
-							<span>Tööriistakuurid</span>
-						</a>
-					</li>
-					<li>
-						<a href="http://profitelgid.ee/est/kodu-aed/gaasigrillid/">
-							<span>BBQ ja gaasigrillid</span>
-						</a>
-					</li>
-				</ul>
-			</li>
-		</ul>
-	</div>
-	<div class="clearfix" />
+            <li>
+              <a href="http://profitelgid.ee/est/telkide-myyk/kasutatud-kuulutused.html">
+                <span>Leiunurk, kasutatud </span>
+              </a>
+            </li>
+            <li>
+              <a href="http://profitelgid.ee/est/telkide-myyk/moodul-pro.html">
+                <span>EasyFlex PRO moodultelgid</span>
+              </a>
+            </li>
+            <li>
+              <a href="http://profitelgid.ee/est/telkide-myyk/profitelk-muuk.html">
+                <span>Tugevad profitelgid</span>
+              </a>
+            </li>
+            <li class="dropdown">
+              <a href="http://profitelgid.ee/est/telkide-myyk/pop-up.html">
+                <span>Pop-up kergtelgid</span>
+              </a>
+              <span class="arrow" lang="6"><img alt="clickable" src="/src/assets/images/menu_bullet.png" style="float:right; transform: rotate(270deg);"></span>
+              <ul>
+                <div class="dropdown-content dropdown-content-2">
+                <li class="">
+                  <a href="http://profitelgid.ee/est/telkide-myyk/pop-up/popup-koduaeda.html">
+                    <span>Home soodne pop-up</span>
+                  </a>
+                </li>
+                <li class="">
+                  <a href="http://profitelgid.ee/est/telkide-myyk/pop-up/soodsad-easyup.html">
+                    <span>Classic Alu kergtelgid</span>
+                  </a>
+                </li>
+                <li class="">
+                  <a href="http://profitelgid.ee/est/telkide-myyk/pop-up/premium-popup.html">
+                    <span>Premium pop-up telgid</span>
+                  </a>
+                </li>
+                <li class="">
+                  <a href="http://profitelgid.ee/est/telkide-myyk/pop-up/kuuskant-hex-profi-popup.html">
+                    <span>PRO seeria popup telgid</span>
+                  </a>
+                </li>
+              </div>
+              </ul>
+            </li>
+            <li>
+              <a href="http://profitelgid.ee/est/telkide-myyk/umartelgid.html">
+                <span>Ümartelgid &amp; star-telgid</span>
+              </a>
+            </li>
+            <li>
+              <a href="http://profitelgid.ee/est/telkide-myyk/peoinventar.html">
+                <span>Peoinventar</span>
+              </a>
+            </li>
+            <li>
+              <a href="http://profitelgid.ee/est/telkide-myyk/telkide-lisavarustus.html">
+                <span>Telkide lisavarustus</span>
+              </a>
+            </li>
+            <li>
+              <a href="http://profitelgid.ee/est/telkide-myyk/teisaldatav-garaaz-telk.html">
+                <span>Teisaldatavad garaaž-telgid</span>
+              </a>
+            </li>
+            <li class="dropdown">
+              <a href="http://profitelgid.ee/est/telkide-myyk/laotelgid.html">
+                <span>Laotelgid &amp; tulekindlad telgid</span>
+              </a>
+              <span class="arrow" lang="6"><img alt="clickable" src="/src/assets/images/menu_bullet.png" style="margin-left: 5px; float:right; transform: rotate(270deg);"></span>
+              <ul>
+                <div class="dropdown-content dropdown-content-2">
+                <li>
+                  <a href="https://profitelgid.ee/est/telkide-myyk/profitelk-muuk.html">
+                    <span>A-tüüpi laotelgid</span>
+                  </a>
+                </li>
+                <li>
+                  <a href="http://profitelgid.ee/est/telkide-myyk/laotelgid/vastupidav-laotelk.html">
+                    <span>B-tüüpi laotelgid</span>
+                  </a>
+                </li>
+              </div>
+              </ul>
+            </li>
+            <li>
+              <a href="http://profitelgid.ee/est/telkide-myyk/pvc-hallid-kaarhallid.html">
+                <span>PVC hallid, kaarhallid</span>
+              </a>
+            </li>
+          </div>
+        </ul>
+      </li>
+      <li>
+        <a href="http://profitelgid.ee/est/vaba-aeg/hobispordi-kaubad.html">
+          Hobispordi kaubad
+        </a>
+      </li>
+      <li>
+        <a href="http://www.profitelgid.ee/est/abs-invest">Kontakt</a>
+      </li>
+      <!--
+        <li>
+          <a href="#">
+            <span>Kodu &amp; aed</span>
+          </a>
+          <ul>
+            <li>
+            <a href="http://profitelgid.ee/est/vaba-aeg/kasvuhooned.html">
+            <span>Kasvuhooned</span>
+            </a>
+            </li>
+            <li>
+              <a href="http://profitelgid.ee/est/vaba-aeg/metall.html">
+                <span>Tööriistakuurid</span>
+              </a>
+            </li>
+            <li>
+              <a href="http://profitelgid.ee/est/kodu-aed/gaasigrillid/">
+                <span>BBQ ja gaasigrillid</span>
+              </a>
+            </li>
+          </ul>
+        </li>
+      -->
+    </ul>
+  </div>
+  <div class="clearfix" />
 </div>
