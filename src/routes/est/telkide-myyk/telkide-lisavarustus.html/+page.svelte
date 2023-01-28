de4d9a3cce1abca4f4d29d468e125a94d5d8468b
@kaspar <team@crewnew.com>
2023-01-28 17:18:39 +0000

    Add new category

---
+++ src/routes/est/telkide-myyk/telkide-lisavarustus.html/+page.svelte
@@ -0,0 +1,62 @@
+<script lang="ts">
+	export let data: any;
+	import { scale } from 'svelte/transition';	
+	console.log(data);
+</script>
+
+<div class="col-main span6" in:scale>
+	<div>
+		<h1>{data.cms.title}</h1>
+		<br />
+	</div>
+	<div class="category-products">
+		<ol class="products-list" id="products-list">
+			{#each data.cms.products as product}
+				<li class="item">
+					<a
+						href="/est/telkide-myyk/laotelgid/soodne-laotelk.html"
+						title="Peoinventari müük"
+						class="product-image"
+					>
+						<img
+							src="http://cms.crewnew.com/assets/{product.image}"
+							class="product"
+							alt="Peoinventari müük"
+						/></a
+					>
+					<div class="product-shop">
+						<div class="f-fix">
+							<h2 class="product-name">
+								<a
+									href="/est/telkide-myyk/laotelgid/soodne-laotelk.html"
+									title="Peoinventari müük"
+								>
+									{product.title}</a
+								>
+							</h2>
+
+							<div class="price-box">
+								<p class="special-price">
+									<span class="price-label"> Hind:</span>
+									<span class="price" id="product-price-101"> {product.selling_price} </span>
+								</p>
+							</div>
+
+						
+							<div class="desc std">
+								{@html product.description}
+							</div>
+						</div>
+					</div>
+				</li>
+			{/each}
+		</ol>
+	</div>
+</div>
+
+<style>
+	.cat_image {
+		float: right;
+		margin-left: 7px;
+	}
+</style>
