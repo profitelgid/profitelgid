d967e2415cf5f6d2478e9508fc739524854e6a10
Kaspar L. Palgi <git@e-stonia.co.uk>
2023-05-12 21:51:30 +0300

    Add privacy page

---
+++ src/routes/est/term/+page.svelte
@@ -0,0 +1,72 @@
+<script lang="ts">
+	import { scale } from 'svelte/transition';
+</script>
+
+
+<svelte:head>
+    <title>Tingimused & Reeglid | PVCfactory</title>
+    <meta name="description" content="Tutvu PVCfactory tingimuste ja reeglitega." />
+</svelte:head>
+
+<div class="col-main span9">
+	<div id="messages_product_view" />
+
+	<div class="product-view" in:scale>
+		<div class="product-essential">
+			<div class="product-shop">
+				<div class="product-name">
+					<h1>Tingimused ja reeglid</h1>
+					<p>&nbsp;</p>
+				</div>
+
+				<ul class="add-to-links" />
+
+				<div class="short-description">
+					<div class="std" in:scale>
+						<h1>Privaatsuspoliitika ning isiklike andmetega töötlemine</h1>    </div><div class="std">
+							<p>&nbsp;</p>
+							<p><strong>Andmete kaitse &amp; privaatsus</strong></p>
+							<p>Meil on karm poliitika, et me ei jaga kunagi mitte mingeid kliendi andmeid mitte &uuml;hegi teise ettev&otilde;ttega. Samuti ei saada me ka ise kunagi r&auml;mpsposti. Kui oled vabatahtlikult ise liitunud meie uudiskirjaga siis saadame maksimaalselt 3 kirja aastas parimate pakkumistega v&otilde;i loosime v&auml;lja lisavarustust. Kui Sa aga pole uudiskirjaga liitunud siis me ei saada Sulle &uuml;htegi kirja mitte kunagi!&nbsp;</p>
+							<p><strong>Turvaline online poodlemine</strong></p>
+							<p>Online tellimused toimuvad turvalises keskkonnas kasutades t&auml;nap&auml;eva tehnika viimast s&otilde;na, et kaitsta Sinu privaatsust. Kasutame turvalisusele suurt r&otilde;hku panevat servermajutuse teenusepakkujat ning meie e-pood on tellitud tipp programmeerijate poolt (<a href="http://e-stonia.ee" target="_blank">e-Stonia.ee</a>).</p>
+					</div>
+				</div>
+
+				<p />
+			</div>
+
+			<div class="product-img-box">
+				<p class="product-image">
+					<img
+						in:scale
+						class="mainImage"
+						style="height:350px"
+						src="https://i.imgur.com/vc2btJP.png"
+						alt="Tugevad profitelgid"
+						title="Tugevad profitelgid"
+					/>
+				</p>
+			</div>
+
+			<div class="clearer" />
+		</div>
+	</div>
+</div>
+
+<style>
+	.productImage {
+		object-fit: cover;
+		width: 65px;
+		height: 65px;
+	}
+	.mainImage {
+		width: 250px;
+		padding: 4em;
+	}
+	.group {
+		padding: 0;
+		border: none;
+		background: none;
+		cursor: pointer;
+	}
+</style>
