9b9be7dc3175b12571f50a00b63616744af96224
Kaspar L. Palgi <git@e-stonia.co.uk>
2023-05-12 21:48:53 +0300

    Fix last front-page broken links

--- src/routes/est/+layout.svelte
+++ src/routes/est/+layout.svelte
@@ -53,19 +53,18 @@
 									</div>
 								</div>
 								<br />
-								<h2>Telkide, lavade, helitehnika ja muu peoinventari m&uuml;&uuml;k ja rent.</h2>
+								<h2>
+									Peotelkide, PVC laotelkide, PVC hallide ja muude PVC toodete müük & tootmine.
+								</h2>
 								<p>
 									<a href="/est/telkide-myyk/profitelk-muuk.html"
-										>Kvaliteetsed &uuml;litugeva raamiga profitelgid,</a
+										>Kvaliteetsed &uuml;litugeva raamiga peotelgid,</a
 									>
 									mida v&otilde;ib tänu paksule PVC kattele aastaringselt kasutada. Alates 2009 müüme
-									ja rendime ka (sealjuures parimate hindadega) erinevaid
-									<a href="/est/telkide-myyk/pop-up.html"
-										>pop-up ehk easy-up kergtelke</a
-									>, soodsamaid
-									<a href="/est/telkide-myyk/koduaeda.html"
-										>kodukasutajale mõeldud telke</a
-									>
+									ja alates 2018 ka toodame (sealjuures kvaliteetseimast PVC materjalist) erinevaid
+									<a href="/est/telkide-myyk/pop-up.html">pop-up ehk easy-up kergtelke</a>,
+									soodsamaid
+									<a href="/est/telkide-myyk/pop-up/popup-koduaeda.html">kodukasutajale mõeldud telke</a>
 									vahel harva telki vajavale inimesele,
 									<a href="/est/telkide-myyk/teisaldatav-garaaz-telk.html"
 										>teisaldatavaid garaaž-telke</a
@@ -73,53 +72,49 @@
 									<a href="/est/telkide-myyk/laotelgid.html"
 										>suuri (tulekindlaid) tööstuslikke halle</a
 									>,
-									<a href="/est/telkide-myyk/umartelgid.html"
-										>star- ja rotund telke</a
-									>
+									<a href="/est/telkide-myyk/umartelgid.html">star- ja rotund telke</a>
 									ning lisaks sellele
-									<a href="/est/telkide-myyk/peoinventar.html"
-										>kõikvõimalikku muud peoinventari</a
-									>.
+									<a href="/est/vaba-aeg/hobispordi-kaubad.html">valikud PVC hobispordi ja vaba aja tooteid.</a>.
 								</p>
 							</div>
 						</div>
 					</div>
 				</div>
 				<Footer />
-                <!--Start of Tawk.to Script-->
-					<script type="text/javascript">
-						var Tawk_API = Tawk_API || {},
-							Tawk_LoadStart = new Date();
-						(function () {
-							var s1 = document.createElement('script'),
-								s0 = document.getElementsByTagName('script')[0];
-							s1.async = true;
-							s1.src = 'https://embed.tawk.to/56578d485ced5f6455f3f0a8/default';
-							s1.charset = 'UTF-8';
-							s1.setAttribute('crossorigin', '*');
-							s0.parentNode.insertBefore(s1, s0);
-						})();
-					</script>
-					<!--End of Tawk.to Script-->
+				<!--Start of Tawk.to Script-->
+				<script type="text/javascript">
+					var Tawk_API = Tawk_API || {},
+						Tawk_LoadStart = new Date();
+					(function () {
+						var s1 = document.createElement('script'),
+							s0 = document.getElementsByTagName('script')[0];
+						s1.async = true;
+						s1.src = 'https://embed.tawk.to/56578d485ced5f6455f3f0a8/default';
+						s1.charset = 'UTF-8';
+						s1.setAttribute('crossorigin', '*');
+						s0.parentNode.insertBefore(s1, s0);
+					})();
+				</script>
+				<!--End of Tawk.to Script-->
 
-					<script>
-						(function (i, s, o, g, r, a, m) {
-							i['GoogleAnalyticsObject'] = r;
-							(i[r] =
-								i[r] ||
-								function () {
-									(i[r].q = i[r].q || []).push(arguments);
-								}),
-								(i[r].l = 1 * new Date());
-							(a = s.createElement(o)), (m = s.getElementsByTagName(o)[0]);
-							a.async = 1;
-							a.src = g;
-							m.parentNode.insertBefore(a, m);
-						})(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
+				<script>
+					(function (i, s, o, g, r, a, m) {
+						i['GoogleAnalyticsObject'] = r;
+						(i[r] =
+							i[r] ||
+							function () {
+								(i[r].q = i[r].q || []).push(arguments);
+							}),
+							(i[r].l = 1 * new Date());
+						(a = s.createElement(o)), (m = s.getElementsByTagName(o)[0]);
+						a.async = 1;
+						a.src = g;
+						m.parentNode.insertBefore(a, m);
+					})(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
 
-						ga('create', 'UA-80333352-1', 'auto');
-						ga('send', 'pageview');
-					</script>
+					ga('create', 'UA-80333352-1', 'auto');
+					ga('send', 'pageview');
+				</script>
 			</div>
 		</div>
 		<!-- end .inner-wrapper-->
