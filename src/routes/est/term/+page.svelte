3a0a05f2b0656d7a9487839bb19e59731c1b91d1
Kaspar L. Palgi <git@e-stonia.co.uk>
2023-05-12 22:01:05 +0300

    Finish terms

--- src/routes/est/term/+page.svelte
+++ src/routes/est/term/+page.svelte
@@ -5,7 +5,7 @@
 
 <svelte:head>
     <title>Tingimused & Reeglid | PVCfactory</title>
-    <meta name="description" content="Tutvu PVCfactory tingimuste ja reeglitega." />
+    <meta name="description" content="Tutvu PVCfactory tingimuste ja reeglitega e-poest ostu sooritamisel." />
 </svelte:head>
 
 <div class="col-main span9">
@@ -23,12 +23,16 @@
 
 				<div class="short-description">
 					<div class="std" in:scale>
-						<h1>Privaatsuspoliitika ning isiklike andmetega töötlemine</h1>    </div><div class="std">
-							<p>&nbsp;</p>
-							<p><strong>Andmete kaitse &amp; privaatsus</strong></p>
-							<p>Meil on karm poliitika, et me ei jaga kunagi mitte mingeid kliendi andmeid mitte &uuml;hegi teise ettev&otilde;ttega. Samuti ei saada me ka ise kunagi r&auml;mpsposti. Kui oled vabatahtlikult ise liitunud meie uudiskirjaga siis saadame maksimaalselt 3 kirja aastas parimate pakkumistega v&otilde;i loosime v&auml;lja lisavarustust. Kui Sa aga pole uudiskirjaga liitunud siis me ei saada Sulle &uuml;htegi kirja mitte kunagi!&nbsp;</p>
-							<p><strong>Turvaline online poodlemine</strong></p>
-							<p>Online tellimused toimuvad turvalises keskkonnas kasutades t&auml;nap&auml;eva tehnika viimast s&otilde;na, et kaitsta Sinu privaatsust. Kasutame turvalisusele suurt r&otilde;hku panevat servermajutuse teenusepakkujat ning meie e-pood on tellitud tipp programmeerijate poolt (<a href="http://e-stonia.ee" target="_blank">e-Stonia.ee</a>).</p>
+						<p><span>See e-tootekataloog / veebisait www.profitelgid.ee (edaspidi "veebisait") on hallatud Innovate Invest O&Uuml; (edaspidi "meie") poolt, mis on Eesti kapitalil p&otilde;hinev ettev&otilde;te. Kasutades meie veebisaiti, siit p&auml;ringuid vormistades v&otilde;i tellimusi esitades eeldame, et oled nende reeglite ja tingimustega tutvunud ja n&otilde;ustud nendega. Innovate Invest O&Uuml;'l on &otilde;igus neid reegleid ja tingimusi muuta igal ajal sellest ette hoiatamata muutes selle lehek&uuml;lje sisu. Osad reeglid ja tingimused, mis on v&auml;lja toodud sellel lehek&uuml;ljel v&otilde;ivad olla eraldi v&auml;lja kirjutatud m&otilde;ne toote lehel ja sellisel juhul see reegel, mis on siin kirjeldatud ei kehti ja kehtib hoopis tootelehel tolle konkreetse toote kohta kirjutatud tingimus.</span></p>
+						<h3>Info meie ja e-poe kohta</h3>
+						<p>See sait on Innovate Invest O&Uuml; omand ja on registreeritud Eesti Vabariigis, Tartus. Firma registreerimisnumber on 14504365 ja pea lao ning registreeritud ametlik postiaadress on Võru 4/6, Tartu 51010. Laos kedagi kogu aeg kohapeal ei ole ning seet&otilde;ttu tuleks alati ette helistada ja kokku leppida. Lao t&ouml;&ouml;taja asub laost 3min kaugusel.</p>
+						<p>T&ouml;&ouml;tame selle nimel, et meie veebisait oleks alati k&otilde;ige v&auml;rskema infoga, uuendatud ning k&otilde;ik lingid t&ouml;&ouml;taks, kuid me ei v&otilde;ta vastutust selle eest, kui m&otilde;nes kohas peaks olema j&auml;&auml;nud sisse viga v&otilde;i on m&otilde;nel tootel j&auml;&auml;nud vananenud info. Palun, anna meile sellest teada, kui kuskil m&otilde;ni link ei t&ouml;&ouml;ta ning me pakume selle eest vastutasuks v&auml;ikest lisasoodustust ning laseme oma veebimeestel vea viivitamatult parandada.</p>
+						<h3>Tarnimine</h3>
+						<p>Anname endast parima, et meie tarnimise hinnad oleksid &uuml;le Eesti soodsad ja seega oleks kogu meie tootevalik v&otilde;imalikult soodne koos tarne hindadega igal pool. Tarne hinna teada saamiseks lisa tooted, mis pakuvad huvi p&auml;ringusse ning vormista see ning Sulle kuvatakse ning saadetakse e-postiga automaatselt kogu summa koos tarnega Sinu aadressile just need tooted, mis valisid.&nbsp;Samuti p&uuml;&uuml;ame tarneajad hoida v&otilde;imalikult l&uuml;hikesed ning vajadusel p&uuml;&uuml;ame samal p&auml;eval tarnida. Innovate Invest O&Uuml; ei vastuta selle eest, kui logistikaettevõtte süül saadetis hilineb. Samuti ei vastuta me selle eest, kui tarnimine viibib vääramatu jõu, tootja, tootja tarnijate või mõne muu kolmanda osapoole tõttu, mida me ei saa kontrollida.</p>
+						<h3><span>Tellimuste t&uuml;histamine</span></h3>
+						<p>Kui oled lisanud tooteid ja/v&otilde;i teenuseid oma hinnap&auml;ringusse v&otilde;i tellimusse ja hiljem registreerunud ning vormistanud siis saadetakse Sulle automaatselt e-postiga hinnapakkumine / arve. Kui Sa seda arvet ei tasu, siis tellimus t&uuml;histatakse automaatselt ning mingeid kohustusi sellega ei kaasne. Samas on Sul v&otilde;imalus kuni 3 kuu jooksul peale vormistamist arve tasuda ning seega tellimus kinnitada.</p>
+						<h3><a name="tagastus"></a>Kauba tagastamine ja garantii</h3>
+						<p>Oleme uhked oma k&otilde;rge kvaliteediga toodete &uuml;le ja alates 2010 aastast, mil alustasime oma tegevust, ei ole meil olnud &uuml;htegi kauba tagastust. Kui toode, mille me m&uuml;&uuml;me ei peaks vastama kirjeldatud kvaliteedile, siis v&otilde;ta meiega &uuml;hendust, et leppida kokku, kuidas saad kauba tagastada ning tagastame kogu summa. Kahe aastane garantii kehtib k&otilde;igile meie m&uuml;&uuml;davatele toodetele. Garantii ei kehti, kui toodet on kahjustatud tulenevalt ebakorrektsest kasutamisest v&otilde;i on toodet kahjustanud ebasobivad ilmastikutingimused. Telkide puhul tuleks kindlasti <a href="/est/telkide-myyk/telkide-lisavarustus.html">soetada erinevaid vahendeid ja jalaraskuseid</a>, et telk kinnitada korralikult pinnase k&uuml;lge, et tuul ei kahjustaks seda. Kui telk peaks aga siiski kannatada saama, siis m&uuml;&uuml;me k&otilde;iki varuosasid k&otilde;ikidele telkidele, mis meie tootevalikus on.</p>
 					</div>
 				</div>
 
@@ -41,7 +45,7 @@
 						in:scale
 						class="mainImage"
 						style="height:350px"
-						src="https://i.imgur.com/vc2btJP.png"
+						src="/images/pvc-terms.png"
 						alt="Tugevad profitelgid"
 						title="Tugevad profitelgid"
 					/>
